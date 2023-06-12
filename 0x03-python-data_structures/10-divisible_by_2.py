a876de0ced18db0d12820803c6a2228085029488
Abraham0625 <adesanya547@gmail.com>
2023-06-12 08:19:19 -0700

    10 and 13

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 # 10-divisible_by_2.py
 
 
@@ -12,3 +12,4 @@ def divisible_by_2(my_list=[]):
             multiples.append(False)
 
     return (multiples)
+
